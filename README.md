d5981bf906de134e0135fdb0df8982508536f3d5
wuying <wuy_123@126.com>
2018-05-03 10:50:56 +0800

    更改readme文件

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 
 #哈尔滨理工大学 小程序分享
+
+#创建git账户
+    *地址:https://github.com
+
+
+#开通微信公众平台账号
+  *地址: https://mp.weixin.qq.com
---
+++ client/pages/index/index.js
@@ -0,0 +1,9 @@
+Page({
+    handleTap: function() {
+        wx.showToast({
+            title: '成功',
+            icon: 'warn',
+            duration: 2000
+        })
+    }
+});
